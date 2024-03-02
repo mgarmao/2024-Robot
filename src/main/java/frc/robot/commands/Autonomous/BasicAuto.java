63305b72513fb1842795f4a230b5dab3ef024c16
mgarmao <mgarmao830@gmail.com>
2024-03-02 14:46:06 -0500

    Adding hopeful fix for stuck not auto

--- src/main/java/frc/robot/commands/Autonomous/BasicAuto.java
+++ src/main/java/frc/robot/commands/Autonomous/BasicAuto.java
@@ -14,6 +14,7 @@ import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
 import frc.robot.commands.StopShooter;
 import frc.robot.commands.Intake.DropIntake;
+import frc.robot.commands.Intake.RaiseIntake;
 import frc.robot.commands.Intake.RunIntake;
 import frc.robot.commands.Intake.StopExtendRetract;
 import frc.robot.commands.Shooter.AutoSmartFire;
@@ -31,6 +32,9 @@ public class BasicAuto extends SequentialCommandGroup {
             new StopShooter(shooter),
             new RunIntake(intake,indexer).withTimeout(1),
             new AutoAbsoluteDrive(swerve,0.5,0.0,0.0,0.0).withTimeout(4),
+            new RaiseIntake(intake).withTimeout(0.3),
+            new DropIntake(intake).withTimeout(0.3),
+            new StopExtendRetract(intake).withTimeout(0.1),
             new AutoAbsoluteDrive(swerve,-0.5,0.0,0.0,0.0).withTimeout(4),
             new AutoSmartFire(shooter, indexer).withTimeout(5),
             new StopShooter(shooter)
